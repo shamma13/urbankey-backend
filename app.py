f5fbac3d02825669dfed965af63abc50238055ca
Shamma Markis <95432232+shamma13@users.noreply.github.com>
2024-04-17 14:39:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,48 +19,50 @@ import os
 
 # UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
 
-
-app = Flask(__name__)
-
-
-
-# app.config.from_pyfile('config.py')  # Load configuration from config.py
-app.config['SECRET_KEY'] = '0622d0d552f33f6309180901'
-app.config['REFRESH_TOKEN_SECRET'] = '062444442d0d554442f33f63091804444901'
-
-#configuring pymongo to work with flask application
-# mongo = PyMongo(app)
-
-# cors_options = {
-#     "origins": ["http://example.com", "http://localhost:3000", "http://localhost:5000"],
-#     "supports_credentials": True
-# }
-
-# app.before_request(credentials_middleware(app))
-
-# cors_options = {
-#     "origins": "http://localhost:3000",
-#     "supports_credentials": True,
-#     "methods": ["GET", "POST"],
-#     "allow_headers": ["Content-Type", "Authorization"],
-#     "options_success_status": 200
-# }
-# CORS(app, **cors_options)
-CORS(app, resources={r"/*": {"origins": "http://localhost:3000", "supports_credentials": True}})
-# CORS(app)
+def create_app():
+    app = Flask(__name__)
 
 
-# Register route blueprints, Blueprint object allows defining a collection of routes (URLs) 
-app.register_blueprint(auth_routes)
-app.register_blueprint(user_routes)
-app.register_blueprint(company_routes)
-app.register_blueprint(financial_routes)
-app.register_blueprint(property_routes)
-app.register_blueprint(reservation_routes)
-app.register_blueprint(unit_routes)
-# Register the upload routes blueprint
-# app.register_blueprint(upload_routes, url_prefix='/uploads')
-app.register_blueprint(upload_routes)
+
+    # app.config.from_pyfile('config.py')  # Load configuration from config.py
+    app.config['SECRET_KEY'] = '0622d0d552f33f6309180901'
+    app.config['REFRESH_TOKEN_SECRET'] = '062444442d0d554442f33f63091804444901'
+
+    #configuring pymongo to work with flask application
+    # mongo = PyMongo(app)
+
+    # cors_options = {
+    #     "origins": ["http://example.com", "http://localhost:3000", "http://localhost:5000"],
+    #     "supports_credentials": True
+    # }
+
+    # app.before_request(credentials_middleware(app))
+
+    # cors_options = {
+    #     "origins": "http://localhost:3000",
+    #     "supports_credentials": True,
+    #     "methods": ["GET", "POST"],
+    #     "allow_headers": ["Content-Type", "Authorization"],
+    #     "options_success_status": 200
+    # }
+    # CORS(app, **cors_options)
+    CORS(app, resources={r"/*": {"origins": "http://localhost:3000", "supports_credentials": True}})
+    # CORS(app)
+
+
+    # Register route blueprints, Blueprint object allows defining a collection of routes (URLs) 
+    app.register_blueprint(auth_routes)
+    app.register_blueprint(user_routes)
+    app.register_blueprint(company_routes)
+    app.register_blueprint(financial_routes)
+    app.register_blueprint(property_routes)
+    app.register_blueprint(reservation_routes)
+    app.register_blueprint(unit_routes)
+    # Register the upload routes blueprint
+    # app.register_blueprint(upload_routes, url_prefix='/uploads')
+    app.register_blueprint(upload_routes)
+
+    return app
 
 
 try:
@@ -70,5 +72,6 @@ except Exception as e:
     print(e)
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
 
