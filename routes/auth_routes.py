11ca2ad3001af9bdec9eb7654ba2bccc1f2eab1e
Shamma Markis <95432232+shamma13@users.noreply.github.com>
2024-04-17 15:52:17 -0400

    Update auth_routes.py

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,7 +1,26 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, render_template_string
 from controllers.Auth import signup, signin, refreshToken, handle_logout #controllers handle incoming requests and they return reponses
 
 auth_routes = Blueprint('auth_routes', __name__) #create blueprint definition
+
+index_html = '''
+        <html>
+
+        <head>
+            <title>WELCOME TO THE BACKEND</title>
+        </head>
+
+        <body>
+
+        </body>
+
+        </html>
+    '''
+
+@auth_routes.route("/")
+def index_route():
+
+    return render_template_string(index_html)
 
 @auth_routes.route("/SignUp", methods=['POST']) #auth_routes decorator defines the signup route with the HTTP request
 def signup_route():
