2f3dd99d746c048ba526e365dde2c548104e3fae
Shamma Markis <95432232+shamma13@users.noreply.github.com>
2024-05-01 01:46:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def create_app():
     #     "options_success_status": 200
     # }
     # CORS(app, **cors_options)
-    CORS(app, resources={r"/*": {"origins": "http://urbankey.s3-website.us-east-2.amazonaws.com/", "supports_credentials": True}})
+    CORS(app, resources={r"/*": {"origins": "https://main--urbankey.netlify.app/", "supports_credentials": True}})
     # CORS(app)
 
 
