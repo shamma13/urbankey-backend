623ae12620c7303630cbd014b4cc421ab307abd5
Shamma Markis <95432232+shamma13@users.noreply.github.com>
2024-05-01 23:08:20 -0400

    Update TokenAuth.py

--- middleware/TokenAuth.py
+++ middleware/TokenAuth.py
@@ -18,10 +18,37 @@ def token_required(f):
         decoded_token = verify_token(token)
         if not decoded_token:
             return jsonify(message='Invalid or expired token'), 403
+        
+        print(decoded_token)
 
         # Attach token payload to request object for easy access in route functions
         request.email = decoded_token.get('email')
         request.role = decoded_token.get('role')
+
+        return f(*args, **kwargs)
+
+    return decorated
+
+def token_required_prime(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        # print(token)
+
+        if not token or not token.startswith('Bearer '):
+            return jsonify(message='Unauthorized token'), 401
+
+        token = token.split(' ')[1]
+
+        decoded_token = verify_token(token)
+        if not decoded_token:
+            return jsonify(message='Invalid or expired token'), 403
+        
+        print(decoded_token)
+
+        # Attach token payload to request object for easy access in route functions
+        request.email = decoded_token[0]
+        request.role = decoded_token[1]
 
         return f(*args, **kwargs)
 
