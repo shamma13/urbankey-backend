46968b0071985e9f0bc9878d1a96f4c8447968b7
Shamma Markis <95432232+shamma13@users.noreply.github.com>
2024-05-02 01:31:47 -0400

    Update TokenAuth.py

--- middleware/TokenAuth.py
+++ middleware/TokenAuth.py
@@ -29,30 +29,30 @@ def token_required(f):
 
     return decorated
 
-def token_required_prime(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = request.headers.get('Authorization')
-        # print(token)
+# def token_required_prime(f):
+#     @wraps(f)
+#     def decorated(*args, **kwargs):
+#         token = request.headers.get('Authorization')
+#         # print(token)
 
-        if not token or not token.startswith('Bearer '):
-            return jsonify(message='Unauthorized token'), 401
+#         if not token or not token.startswith('Bearer '):
+#             return jsonify(message='Unauthorized token'), 401
 
-        token = token.split(' ')[1]
+#         token = token.split(' ')[1]
 
-        decoded_token = verify_token(token)
-        if not decoded_token:
-            return jsonify(message='Invalid or expired token'), 403
+#         decoded_token = verify_token(token)
+#         if not decoded_token:
+#             return jsonify(message='Invalid or expired token'), 403
         
-        print(decoded_token)
+#         print(decoded_token)
 
-        # Attach token payload to request object for easy access in route functions
-        request.email = decoded_token[0]
-        request.role = decoded_token[1]
+#         # Attach token payload to request object for easy access in route functions
+#         request.email = decoded_token[0]
+#         request.role = decoded_token[1]
 
-        return f(*args, **kwargs)
+#         return f(*args, **kwargs)
 
-    return decorated
+#     return decorated
 
 def generate_access_token(email, role):
     """Generate an access token for the given user."""
